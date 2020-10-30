a66462e739cbc027f8223ec8ba62c825436ffd66
xTharc <Tharc@users.noreply.github.com>
2020-10-30 19:41:45 +0100

    Version_0.2

--- app.js
+++ app.js
@@ -12,45 +12,81 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-//
-mongoose.connect("mongodb://admin:PrOtoniXX10878@localhost:27017/nadeDB", {
-  useNewUrlParser: true
+//mongodb://admin:PrOtoniXX10878@localhost:27017/nadeDB
+mongoose.connect("mongodb://localhost:27017/phoneDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 // ----------------------------------MONGODB SCHEMAS AND MODULES----------------
-const pageSchema = new mongoose.Schema ({
-  mapname: String,
-  content: String,
-
-
+const phoneSchema = new mongoose.Schema ({
+  phoneName: String,
+  phonePic: String,
+  fixTypes: [String],
+  phonePrice: [String],
+  order: String
 });
-const ipSchema = new mongoose.Schema ({
-  ip: String
-});
-const dataSchema = new mongoose.Schema ({
-  spot: String,
-  timesclicked: Number
-});
+const Phones = new mongoose.model("Phones", phoneSchema);
 
-const Mapp = new mongoose.model("Mapp", pageSchema);
-const Ipp = new mongoose.model("Ipp", ipSchema);
-const Dataa = new mongoose.model("Dataa", dataSchema);
-//
-//  var inputin = new Mapp({
-//   mapname: "titan",
-//   content: "",
-//
-//
+// const ipSchema = new mongoose.Schema ({
+//   ip: String
 // });
-// inputin.save();
+// const dataSchema = new mongoose.Schema ({
+//   spot: String,
+//   timesclicked: Number
+// });
+//
+//const Mapp = new mongoose.model("Mapp", pageSchema);
+// const Ipp = new mongoose.model("Ipp", ipSchema);
+// const Dataa = new mongoose.model("Dataa", dataSchema);
+// //
+ var inputin = new Phones({
+   phoneName: "iPhone 8S",
+   phonePic: "https://imgaz.staticbg.com/thumb/large/oaupload/banggood/images/A2/F5/587b03e3-bc3e-402e-bdbd-27d05b6620ad.jpg",
+   fixTypes: ["Popravilo Stekla","Menjava baterije"],
+   phonePrice: ["20","30"]
+});
+
+
 
 //-------------------------------------OUR APP----------------------------------
 
 
 app.get("/", function(req, res) {
+  Phones.find({},function(err,foundPhones){
 
-
+console.log(foundPhones);
+      res.render("index", {
+        phones: foundPhones
+      });
 
 });
+});
+
+app.get("/admin", function(req, res) {
+  Phones.find({},function(err,foundPhones){
+
+console.log(foundPhones);
+      res.render("admin", {
+        phones: foundPhones
+      });
+
+});
+});
+
+app.post("/admin", function(req, res) {
+  const newPhone = new Phones({
+    phoneName: req.body.phoneName,
+    phonePic: req.body.phonePic,
+    fixTypes: req.body.fixTypes,
+    phonePrice: req.body.fixPrice
+    });
+
+  newPhone.save();
+
+  res.redirect("/admin");
+});
+
+
 
 //  <%-map.content%>
 
@@ -58,7 +94,7 @@ app.get("/", function(req, res) {
 
 
 //------------------------------------------------------------------------------
-var server1 = app.listen(3000, function() {
+var server1 = app.listen(4000, function() {
   console.log("Server started on port 3000");
 });
 
